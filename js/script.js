4367394667c5092a6b96eabbc2165df19fc1eb3e
Ivan <Ivan>
2023-02-20 11:27:03 +0500

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// alert('hello alert');
+
+// const result = confirm('are you here');
+// console.log(result);
+
+// // const answer = prompt('vam yest 18?' , '18');
+// console.log(typeof(answer))
+// const answer = +prompt('vam yest 18?' , '18');
+// console.log(answer + 5)
+
+// const answers = [];
+
+// answers[0] = prompt('kak vashe imya?', '');
+// answers[1] = prompt('kak vashe familiya?', '');
+// answers[2] = prompt('skolko vam let?', '');
+// console.log(typeof(answers))
+// console.log(typeof(null))
+
+
+// // -----------------Интерполяция------------------------------------
+// const category = 'toys';
+
+// console.log('https://someurl.com/' + category + '/' + '4');
+// console.log(`https://someurl.com/${category}/5`); 
+
+// const user = 'Ivan'
+// alert(`Privetm ${user}`)
+
+
+// // ----------------------- Операторы----------------------------------
+
+// console.log('arr' + ' - object');
+// console.log(4 + + ' - object');
+// console.log(4 + + ' 4');
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+ 
+// console.log(incr++);
+// console.log(decr--);
+
+
+// console.log(2*4 == 8)
+
+// console.log(5%2)
+
+
+// && И
+// || Или
+
+// const isChecked = false,
+//       isClose = false;
+
+// console.log(isChecked && isClose)
+// console.log(isChecked || !isClose);
+
+
+// console.log(2 + 2 * 2 != 8);
+
+
+
+
+
+
